1ad6d8ce7be2f407304de5ce120ae78b712edd9a
fathimathul naja apm <fathimathulnajaapm@gmail.com>
2024-03-13 20:30:20 +0530

    fourth commit

--- portfolio2/src/components/Hero/CTA.jsx
+++ portfolio2/src/components/Hero/CTA.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import CV from './cv.pdf'
 
 export const CTA = () => {
   return (
     <div className='cta'> 
 <span >
-    <a href="download my cv.pdf"  className='btn'>Download CV</a>
+    <a href={CV} className='btn'>Download CV</a>
     <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </span>
     </div>
